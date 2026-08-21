e1d907925f84de8d4675023f7b22e40e1da0be8e
Matej Milankov <matej.milankov@gmail.com>
2026-08-21 17:47:20 +0200

    Included GSAP and implemented animation for search bar

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,27 @@
 import { Header } from "../../components/Header/Header"
+import { useEffect } from "react"
+import axios from "axios"
 
 export function Home() {
+    
+    useEffect(() => {
+        const getMovies = async () => {
+            const response = await axios.get('https://api.themoviedb.org/3/search/movie',
+                {
+                    params: {
+                        query: 'Batman'
+                    },
+                    headers: {
+                        Authorization: `Bearer ${import.meta.env.VITE_TMDB_ACCESS_TOKEN}`
+                    }
+                }
+            );
+            console.log(response.data.results);
+        }
+
+        getMovies();
+    }, []);
+
     return (
         <>
             <Header />
