ea547a30b047a52e51512c41ecf5d0916c032be2
Matej Milankov <matej.milankov@gmail.com>
2026-08-25 18:42:17 +0200

    Included Swiper js library

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,27 @@
 import { Header } from "../../components/Header/Header"
+import { Swiper, SwiperSlide } from "swiper/react"
+import { Navigation, Autoplay } from "swiper/modules";
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
 export function Home() {
 
     return (
         <>
             <Header />
-            <h1>Welcome to IMDB</h1>
+            <Swiper
+                modules={[Navigation, Autoplay]}
+                spaceBetween={30}
+                slidesPerView={1}
+                navigation
+                pagination={{ clickable: true }}
+                autoplay={{ delay: 3000 }}
+            >
+                <SwiperSlide style={{ height: '300px', background: 'red' }}>Slide 1</SwiperSlide>
+                <SwiperSlide style={{ height: '300px', background: 'blue' }}>Slide 2</SwiperSlide>
+            </Swiper>
         </>
     )
 }
