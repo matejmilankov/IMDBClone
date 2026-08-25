7ea5e638cf990410605cc12fa667e84281b6e88a
Matej Milankov <matej.milankov@gmail.com>
2026-08-25 19:38:38 +0200

    Initial Swiper js slider in hero section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,29 @@
 import { Header } from "../../components/Header/Header"
 import { Swiper, SwiperSlide } from "swiper/react"
 import { Navigation, Autoplay } from "swiper/modules";
+import { useEffect } from "react";
+import axios from "axios";
 
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 
 export function Home() {
+
+    useEffect(() => {
+        const getNowPlayingMovies = async () => {
+            const response = await axios.get('https://api.themoviedb.org/3/movie/now_playing',
+                {
+                    headers: {
+                        Authorization: `Bearer ${import.meta.env.VITE_TMDB_ACCESS_TOKEN}`
+                    }
+                }
+            );
+            console.log(response.data.results);
+        }
+
+        getNowPlayingMovies();
+    }, []);
 
     return (
         <>
@@ -19,8 +36,8 @@ export function Home() {
                 pagination={{ clickable: true }}
                 autoplay={{ delay: 3000 }}
             >
-                <SwiperSlide style={{ height: '300px', background: 'red' }}>Slide 1</SwiperSlide>
-                <SwiperSlide style={{ height: '300px', background: 'blue' }}>Slide 2</SwiperSlide>
+                <SwiperSlide style={{ height: '300px'}}>Slide 1</SwiperSlide>
+                <SwiperSlide style={{ height: '300px'}}>Slide 2</SwiperSlide>
             </Swiper>
         </>
     )
