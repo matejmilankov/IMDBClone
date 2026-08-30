e6e6857c63ad811df5cbdf04ee1b99bd4acc2c8b
Matej Milankov <matej.milankov@gmail.com>
2026-08-30 15:43:17 +0200

    Styled watchlist icon

--- src/components/Icons/Icons.jsx
+++ src/components/Icons/Icons.jsx
@@ -42,12 +42,23 @@ export function BigWatchlistIcon({ bookmarkWidth, bookmarkHeight, plusWidth, plu
                     <polygon fill={fill} points="24 0 0 0 0 32 12.2436611 26.2926049 24 31.7728343"></polygon>
                     <polygon fill="rgba(0, 0, 0, 0.6)" points="24 31.7728343 24 33.7728343 12.2436611 28.2926049 0 34 0 32 12.2436611 26.2926049"></polygon>
                 </svg>
-                <svg xmlns="http://www.w3.org/2000/svg" width={plusWidth} height={plusHeight} viewBox="0 0 24 24" fill="#fff" role="presentation">
-                    <path d="M18 13h-5v5c0 .55-.45 1-1 1s-1-.45-1-1v-5H6c-.55 0-1-.45-1-1s.45-1 1-1h5V6c0-.55.45-1 1-1s1 .45 1 1v5h5c.55 0 1 .45 1 1s-.45 1-1 1z"></path>
-                </svg>
+                {fill === '#F5C518' ? (
+                    <CheckIcon />
+                ) : (
+                    <svg xmlns="http://www.w3.org/2000/svg" width={plusWidth} height={plusHeight} viewBox="0 0 24 24" fill="#fff" role="presentation">
+                        <path d="M18 13h-5v5c0 .55-.45 1-1 1s-1-.45-1-1v-5H6c-.55 0-1-.45-1-1s.45-1 1-1h5V6c0-.55.45-1 1-1s1 .45 1 1v5h5c.55 0 1 .45 1 1s-.45 1-1 1z"></path>
+                    </svg>
+                )
+                }
             </div>
         </div>
     );
+}
+
+export function CheckIcon() {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="#000" role="presentation"><path fill="none" d="M0 0h24v24H0V0z"></path><path d="M9 16.2l-3.5-3.5a.984.984 0 0 0-1.4 0 .984.984 0 0 0 0 1.4l4.19 4.19c.39.39 1.02.39 1.41 0L20.3 7.7a.984.984 0 0 0 0-1.4.984.984 0 0 0-1.4 0L9 16.2z"></path></svg>
+    )
 }
 
 export function ProfileIcon() {
