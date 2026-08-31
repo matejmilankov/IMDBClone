f0456ab05e97501b2b0b2449fec51baefabf443b
Matej Milankov <matej.milankov@gmail.com>
2026-08-31 18:07:42 +0200

    Fixed bugs

--- src/components/Menu/menuItems.js
+++
@@ -1,9 +0,0 @@
-import { LogoIcon, CloseIcon, MovieIcon, TvIcon, WatchIcon, EventsIcon, UserIcon, GlobeIcon } from "../Icons/Icons";
-
-export const menuItems = [
-    {
-        icon: MovieIcon,
-        title: 'Movies',
-        links: ['Release calendar', 'Top 250 movies', 'Most popular movies', 'Browse movies by gender', 'Top box office', 'Showtimes & tickes', 'Movie news', 'India movie spotlight']
-    }
-]
