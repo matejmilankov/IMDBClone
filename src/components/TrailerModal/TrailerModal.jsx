8645efb33be87b44c6f111cfaef16a28cb0445d6
Matej Milankov <matej.milankov@gmail.com>
2026-09-02 00:42:00 +0200

    Included react-youtube package for trailers

--- src/components/TrailerModal/TrailerModal.jsx
+++ src/components/TrailerModal/TrailerModal.jsx
@@ -1,6 +1,40 @@
 import { useMovieTrailer } from "../../hooks/useMovieTrailer";
+import { useRef } from "react";
+import { useGSAP } from "@gsap/react";
+import gsap from 'gsap';
 
 export function TrailerModal({ clickedTrailerId }) {
     const { trailer, error, isLoading } = useMovieTrailer(clickedTrailerId);
     console.log(trailer);
+
+    const blurOverlayRef = useRef(null);
+
+    useGSAP(() => {
+        gsap.to(blurOverlayRef.current, {
+            opacity: 1,
+            pointerEvents: 'auto',
+            duration: 0.3
+        });
+    });
+
+    const closeOverlay = () => {
+        gsap.to(blurOverlayRef.current, {
+            opacity: 0,
+            pointerEvents: 'none',
+            duration: 0.3
+        });
+    }
+
+    return (
+        <>
+            {isLoading && <p>Loading...</p>}
+            {error && <p>Something went wrong. Please try again later.</p>}
+            <div 
+                className='backdropOverlay visible' 
+                ref={blurOverlayRef} 
+                onClick={closeOverlay}
+            />
+
+        </>
+    )
 }
