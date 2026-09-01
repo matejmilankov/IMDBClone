a7edef7f2a8f93ecc2acfce1a0385d68b9f4e034
Matej Milankov <matej.milankov@gmail.com>
2026-09-01 19:21:30 +0200

    Implemented heading for section as component

---
+++ src/components/SectionHeader/SectionHeader.jsx
@@ -0,0 +1,59 @@
+import styles from './SectionHeader.module.css'
+import clsx from 'clsx'
+import { ArrowIcon } from '../Icons/Icons'
+
+export function SectionHeader(props) {
+    console.log(props.variant)
+    return (
+        <div className='container'>
+            <div className={clsx(
+                styles.headerLayout,
+                {
+                    [styles.extended]: props.variant === "extended",
+                    [styles.reduced]: props.variant === "reduced",
+                    [styles.standard]: props.variant === "standard"
+                })}
+            >
+                {props.variant === "standard" && (
+                    <div className={styles.sectionPreHeader}>
+                        <h2>{props.preHeader}</h2>
+                        <div>
+                            <span>{props.preHeaderLink}</span>
+                            <ArrowIcon
+                                width={"10px"}
+                                height={"10px"}
+                                color={"#0E63BE"}
+                            />
+                        </div>
+                    </div>
+                )}
+
+                <div className={styles.sectionHeaderWrapper}>
+                    <div>
+                        <div className={styles.sectionIndicator}>
+                            <div className={styles.line}></div>
+                            <h3>{props.header}</h3>
+                            <ArrowIcon
+                                width={"18px"}
+                                height={"18px"}
+                                color={"#fff"}
+                            />
+                        </div>
+                        <span className={styles.sectionHeaderDesc}>{props.headerDesc}</span>
+                    </div>
+                    {props.variant === "extended" && (
+                        <button className={styles.sectionAction}>
+                            <ArrowIcon
+                                width={"10px"}
+                                height={"10px"}
+                                color={"#0E63BE"}
+                            />
+                            <span>{props.actionText}</span>
+                        </button>
+                    )}
+                </div>
+
+            </div>
+        </div>
+    )
+}
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import { Header } from "../../components/Header/Header"
 import { HeroSlider } from "../../components/HeroSlider/HeroSlider";
 import { useNowPlayingMovies } from "../../hooks/useNowPlayingMovies";
+import { SectionHeader } from "../../components/SectionHeader/SectionHeader";
 import styles from './Home.module.css'
 
 import 'swiper/css';
@@ -17,6 +18,14 @@ export function Home() {
                 {isLoading && <p>Loading...</p>}
                 {error && <p>Something went wrong. Please try again later.</p>}
                 <HeroSlider heroMovies={heroMovies}/>
+                <SectionHeader 
+                    variant="standard"
+                    preHeader="What to watch"
+                    preHeaderLink="Get more recommendations"
+                    header="Top picks"
+                    headerDesc="Tv shows and movies just for you"
+                />
+                <section style={{margin:"200px 0"}}></section>
             </div>
         </>
     )
